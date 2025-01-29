161ba4d70ba010e6c5bfba2895b569401014b8d2
Jasmine Aguilar <jasmineaguilar@mail.smu.edu>
2025-01-28 18:01:26 -0600

    Added css and js files and added more html pages

---
+++ js/index.js
@@ -0,0 +1,9 @@
+"use strict";
+
+(() => {
+document.querySelector("#testButton").addEventListener("click", handleClick);
+
+function handleClick() {
+    console.log("No thanks for clicking!");
+}
+})();
