ef892fb70ea8297c8a792f4d58a17d5d021cd472
Jasmine Aguilar <jasmineaguilar@mail.smu.edu>
2025-02-13 20:19:08 -0600

    spiffing contact page with my particular language

--- app.js
+++ app.js
@@ -1,13 +1,19 @@
 import express from "express";
+import dotenv from "dotenv";
+dotenv.config();
+
 const app = express();
 const port = 3000;
-
+app.use(express.json());
 app.use(express.static("public"));
+
+
 
 app.post("/mail", (req, res) => {
     res.send("mail button clicked");
 });
 
 app.listen(port, () => {
+    console.log(process.env.SENSITIVE_INFO)
     console.log(`Example app listening on port ${port}`);
 });
---
+++ utils/utils.js
@@ -0,0 +1,6 @@
+import nodemailer from "nodemailer";
+
+function sendMessage(sub, txt) {
+
+
+}
