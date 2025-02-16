1a3ff36b130357e0a781842144458ed05a278fc4
Jasmine Aguilar <jasmineaguilar@mail.smu.edu>
2025-02-16 17:06:28 -0600

    troubleshooting missing body and feedback.

--- utils/utils.js
+++ utils/utils.js
@@ -3,13 +3,13 @@ import nodemailer from "nodemailer";
 export async function sendMessage(sub, txt) {
     let transporter = nodemailer.createTransport({
         host: process.env.GMAIL_HOST,
-        port: process.env.MAIL_PORT, // Should be 587 for TLS
-        secure: false, // Should be false for TLS (not SSL)
+        port: process.env.MAIL_PORT,
+        secure: false,
         auth: {
             user: process.env.GMAIL_USERNAME,
             pass: process.env.GMAIL_PASSWORD,
         },
-        requireTLS: process.env.MAIL_TLS === "true", // Ensure TLS is required
+        requireTLS: process.env.MAIL_TLS === "true", 
     });
 
     let message = {
@@ -19,11 +19,13 @@ export async function sendMessage(sub, txt) {
         text: txt,
     };
 
-    try {
-        await transporter.sendMail(message);
-        console.log("Message sent");
-    } catch (err) {
-        console.log("Message not sent - " + err);
-        throw err;  // Re-throwing error
+    await transporter
+        .sendMail(message)
+        .then(() => {
+            console.log("Message sent");
+        })
+        .catch((err) => {
+            console.log("Message not sent - " + err);
+        throw err});  
+
     }
-}
